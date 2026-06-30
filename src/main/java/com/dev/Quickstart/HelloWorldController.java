a7e7a57c212f8ff5e791235219c400adc79ba75d
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-30 23:58:00 +0300

    in new class if path = --- return that class

--- src/main/java/com/dev/Quickstart/HelloWorldController.java
+++ src/main/java/com/dev/Quickstart/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    
+    @GetMapping(path = "/hello")
     public String helloWorld(){
         return "helloWorld";
     }
