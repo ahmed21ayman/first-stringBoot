a80ce5bb7657cc190ec2448653c7b506014bc74b
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-30 23:57:27 +0300

    in new class add return string

--- src/main/java/com/dev/Quickstart/HelloWorldController.java
+++ src/main/java/com/dev/Quickstart/HelloWorldController.java
@@ -1,8 +1,12 @@
 package com.dev.Quickstart;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
     
+    public String helloWorld(){
+        return "helloWorld";
+    }
 }
