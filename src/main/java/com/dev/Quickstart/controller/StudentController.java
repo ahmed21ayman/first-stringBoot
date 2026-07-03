3c23524c9b134c75c3751a18eb6b8a67c62f63a5
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-07-03 16:47:20 +0300

    v2.0: add request mapping folder

--- src/main/java/com/dev/Quickstart/controller/StudentController.java
+++ src/main/java/com/dev/Quickstart/controller/StudentController.java
@@ -1,12 +1,18 @@
 package com.dev.Quickstart.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class StudentController {
     @GetMapping("/students")
     public String allStudents(){
         return "all data";
+    }
+    @GetMapping("/student")
+    public String oneStudent(){
+        return "student one";
     }
 }
