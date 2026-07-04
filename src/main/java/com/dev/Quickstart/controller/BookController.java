41469765615b658696e63d5457894720ff3632b5
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-07-04 19:33:55 +0300

    v5: get student using id

--- src/main/java/com/dev/Quickstart/controller/BookController.java
+++ src/main/java/com/dev/Quickstart/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,10 @@ public class BookController {
     public Book getOneBook(){
         Book oneBook = new Book("ahmed", "lela w lela", 50);
         return oneBook;
+    }
+    @GetMapping
+    public String getStudent(@PathVariable int id){
+        return "student id = "+ id;
     }
     @PostMapping("")
     public Book createBook(@RequestBody Book book){
