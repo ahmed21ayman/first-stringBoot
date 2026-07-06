99d326d2e70407d8e4f8df8030c70c05fd93c05e
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-07-07 01:48:52 +0300

    v14: create repo folder & file and start edit on serviece to connect with repo

--- src/main/java/com/dev/Quickstart/service/StudentService.java
+++ src/main/java/com/dev/Quickstart/service/StudentService.java
@@ -1,11 +1,15 @@
 package com.dev.Quickstart.service;
 
 import org.springframework.stereotype.Service;
-
 import com.dev.Quickstart.model.Student;
+import com.dev.Quickstart.repository.StudentRepository;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+    public StudentService(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+}
     public Student getTopStudent(){
         return new Student("Ahmed", 23);
     }
